856f3f6e9af2ce927af6c4af4915e50ad84da6d8
shimura120 <x19050xx@aitech.ac.jp>
2020-12-16 13:26:42 +0900

    ログイン＆アカウント作成のUI
    ログイン画面と新規アカウント作成画面のUIのみ作成しました

---
+++ web.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, render_template, url_for
+import os
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/create_account/')
+def create_account():
+    return render_template('create_account.html')
+
+
+@app.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
+
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path,
+                                     endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
+
+
+if __name__ == '__main__':
+    app.run(host="localhost", port=8080)
