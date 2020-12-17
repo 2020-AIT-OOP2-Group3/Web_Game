635849d25bdb7bdc8b60b26f543149e442b2edb7
shinohara-hideki <shinoharahideki3@gmail.com>
2020-12-17 14:21:36 +0900

    web-game 新規登録01

--- web.py
+++ web.py
@@ -1,5 +1,6 @@
-from flask import Flask, request, render_template, url_for
+from flask import Flask, request, render_template, url_for, jsonify
 import os
+import json
 
 app = Flask(__name__)
 
@@ -7,12 +8,6 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
-
-
-@app.route('/create_account/')
-def create_account():
-    return render_template('create_account.html')
-
 
 @app.context_processor
 def override_url_for():
@@ -27,6 +22,43 @@ def dated_url_for(endpoint, **values):
                                      endpoint, filename)
             values['q'] = int(os.stat(file_path).st_mtime)
     return url_for(endpoint, **values)
+
+
+@app.route('/create_account')
+def create_account():
+    return render_template('create_account.html')
+
+
+#新規登録
+@app.route('/add_account', methods=["POST"])
+def add_account():
+
+    #新規ユーザ情報の取得
+    y_name = request.form.get('nickname', None)
+    y_email = request.form.get('email', None)
+    y_pass = request.form.get('password', None)
+
+    #player.jsonを開き、json_dataに格納
+    with open('player.json') as f:
+        json_data = json.load(f)
+
+    #新規ユーザの情報をそれぞれ変数に格納
+    item = {}
+    item["id"] = y_email
+    item["pass"] = y_pass
+    item["point"] = 0
+    item["name"] = y_name
+    json_data.append(item)
+
+    #idが重複したかの確認
+
+    #player.jsonに書き込み
+    with open('player.json', 'w') as f:
+        json.dump(json_data, f, ensure_ascii=False, indent=4, sort_keys=True, separators=(',', ': '))
+
+    return jsonify({
+        "status": "append completed"
+    })
 
 
 if __name__ == '__main__':
