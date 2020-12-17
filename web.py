d62816583d9b47e7455e8b82e44e73e60da961a8
ItsukiMatsunaga <69783579+ItsukiMatsunaga@users.noreply.github.com>
2020-12-17 14:27:27 +0900

    インデント微調整＆コメント追加

--- web.py
+++ web.py
@@ -9,27 +9,7 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.context_processor
-def override_url_for():
-    return dict(url_for=dated_url_for)
-
-
-def dated_url_for(endpoint, **values):
-    if endpoint == 'static':
-        filename = values.get('filename', None)
-        if filename:
-            file_path = os.path.join(app.root_path,
-                                     endpoint, filename)
-            values['q'] = int(os.stat(file_path).st_mtime)
-    return url_for(endpoint, **values)
-
-
-@app.route('/create_account')
-def create_account():
-    return render_template('create_account.html')
-
-
-#新規登録
+#ーーーーーーーーーーーーーーーーーーーーーーーー新規登録ーーーーーーーーーーーーーーーーーーーーーー#
 @app.route('/add_account', methods=["POST"])
 def add_account():
 
@@ -61,5 +41,73 @@ def add_account():
     })
 
 
+#ーーーーーーーーーーーーーーーーーーーーーーーーメールアドレスとパスワードの取得ーーーーーーーーーーーーーーーーーーーーーー#
+@app.route('/login', methods=["GET"])
+def login():
+
+    # メールアドレスとパスワードの取得
+    l_id = request.args.get("email")
+    l_pas = request.args.get("password")
+
+    print(f"login id = {l_id} , password = {l_pas}")
+
+    with open('player.json') as f:  # jsonファイルの読み込み
+        json_data = json.load(f)
+
+    for i in json_data:
+        print(i)
+        if i["id"] == l_id:
+            print("id O")
+            if i["pas"] == l_pas:  # メールアドレスとパスワードが一致していたらログインしてゲーム画面へ
+                print(f"pas O ,i:{i}")
+                return render_template('.html',  # ゲーム画面のHTML
+                                        point=i["point"],
+                                        name=i["name"])
+            else:
+                print("pas X")
+                err = "IDとパスワードが一致しません"  # IDは存在するがパスワードが合っていない場合
+                return render_template('index.html',
+                                        err=err)
+
+    print("ID X")
+    err = "登録されていないIDです"  # IDが見つからなかった場合
+    return render_template('create_account.html',
+                            err=err)
+
+
+
+@app.route('/create_account/')
+def create_account():
+    return render_template('create_account.html')
+
+
+@app.route('/menu/')
+def menu():
+    return render_template('menu.html')
+
+
+@app.route('/menu/', methods=['POST'])
+def menu_POST():
+    return render_template('menu.html')
+
+@app.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
+
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path,
+                                    endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
+
+
+@app.route('/create_account')
+def create_account():
+    return render_template('create_account.html')
+
 if __name__ == '__main__':
-    app.run(host="localhost", port=8080)
+    app.run(host="localhost", port=8080, debug=True)
