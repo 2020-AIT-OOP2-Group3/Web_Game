a600728abd357ae5ad3ee2fe37772d4b84c7b73e
shinohara-hideki <shinoharahideki3@gmail.com>
2020-12-28 10:56:54 +0900

    じゃんけん脳トレ　スタート画面(途中経過)

--- web.py
+++ web.py
@@ -260,7 +260,7 @@ def dated_url_for(endpoint, **values):
 def create_account():
     return render_template('create_account.html')
 
-#ーーーーーーーーーーーーーーーーーーーーーーーー脳トレーーーーーーーーーーーーーーーーーーーーーー#
+#ーーーーーーーーーーーーーーーーーーーーーーーーじゃんけん脳トレーーーーーーーーーーーーーーーーーーーーーー#
 #  脳トレ -脳トレメニュー-
 @app.route('/notore_menu', methods=["GET"])
 def notore_menu():
@@ -273,7 +273,9 @@ def notore_menu():
 
 @app.route('/janken/start/', methods=["GET"])
 def janken_start():
-    return render_template('janken_notore/janken_start.html')
+    return render_template('janken_notore/janken_start.html',
+                            point=session["point"],
+                            name=session["name"])
 
 # じゃんけん脳トレ -プレイページ-
 
